1dfd3de25185560d02d0ed831c46edc9f50fb576
Lorhwuene <llapuz685@gmail.com>
2026-09-22 08:37:58 +0800

    Added sql dump for 09_22_2026

--- index.php
+++ index.php
@@ -1,118 +1,155 @@
-<?php
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 22, 2026 at 02:34 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
-//Database Connection
-$host = "localhost';
-$db =   'library_db';
-$user = 'root';
-$pass = '';
-$charset = 'utf8mb4';
-
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-]
-try{
-
-    $pdo = new PDO($dsn, $user, $pass, $options);
-    echo 'connection successful';
-} catch (\PDOException $e) {
-    die("Database connection failed" . $e->getMessage());
-}
-
-// Session
-session_start();
-
-//Determine the current section
-$section = $_GET['section'] ?? 'students';
-
-// Determine the CRUD operation
-$action = $_GET['action'] ?? '';
-
-// Fetch Students
-if($section==='students'){
-
-    $stmt = $pdo->("
-SELECT * 
-FROM students
-ORDER BY student_id DESC
-");
-
-$students = $stmt->fetchAll();
-}
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-?>
-<DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Library System</title>
-</head>
-<body>
-    <hi>Simple Library System</h1>
-    <nav>
-        <a href="index.php?section=studentsstudents">Students</a> 
-        <A href="index.php?section=books">Books</a>
-        <a href="index.php?section=borrow">Borrow</a>
-    </nav>
-    <hr>
-    <?php if($section === 'students'): ?>
-        <h1>Students</h1>
-        <table>
-            <thead>
-                <tr>
-                    <th></th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Course</th>
-                    <th>Created At</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach($students as student): ?>
-                    <tr>
-                        <td>
-                            <?=htmlspecialchars($student['student_id'])?>
-                        </td>
-                    <tr>
-                <thead>
-                <tbody>
-                    <?php foreach($students as $student): ?>
-                        <tr>
-                            <td><?=htmlspecialchars($student['student_id'])?></td>
-                            <td><?=htmlspecialchars($student['first_name'])?></td>
-                            <td><?=htmlspecialchars($student['last_name'])?></td>
-                            <td><?=htmlspecialchars($student['course'])?></td>
-                            <td><?=htmlspecialchars($student['created_at'])?></td>
-                            <td>
-                                <a>Edit</a>
+--
+-- Database: `it30b_lab_db`
+--
 
-                                <a>Delete</a>
-                            </td>
-                        </tr>
-                    <?php endforeach; ?>
-                </tbody>
+-- --------------------------------------------------------
 
-            </table>
+--
+-- Table structure for table `books`
+--
 
+CREATE TABLE `books` (
+  `book_id` int(11) NOT NULL,
+  `book_title` varchar(100) NOT NULL,
+  `book_author` varchar(100) NOT NULL,
+  `book_category` varchar(50) NOT NULL,
+  `book_created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-            
-    <?php endif; ?>
+--
+-- Dumping data for table `books`
+--
 
-    <?php if($section === 'books'): ?>
-        <h1>Books</h1>
-    <?php endif; ?>
+INSERT INTO `books` (`book_id`, `book_title`, `book_author`, `book_category`, `book_created_at`) VALUES
+(1, 'Slow Productivity', 'Cal Newport', 'Non Fiction', '2026-09-21 23:28:00'),
+(2, 'Jurassic Park', 'Michael Crichton', 'Science Fiction', '2026-09-21 23:28:00'),
+(3, '1984', 'George Orwell', 'Science Fiction', '2026-09-21 23:28:00');
 
-    <?php if($section === 'borrow'): ?>
-        <h1>Borrow</h1>
-    <?php endif; ?>
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `borrow`
+--
 
-</body>
-</html>
+CREATE TABLE `borrow` (
+  `borrow_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL,
+  `borrow_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `borrow_return_date` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `borrow`
+--
+
+INSERT INTO `borrow` (`borrow_id`, `student_id`, `book_id`, `borrow_date`, `borrow_return_date`) VALUES
+(1, 1, 2, '2026-09-21 23:28:00', NULL),
+(2, 2, 1, '2026-09-21 23:28:00', NULL),
+(3, 3, 3, '2026-09-21 23:28:00', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `student_id` int(11) NOT NULL,
+  `student_first_name` varchar(50) NOT NULL,
+  `student_last_name` varchar(50) NOT NULL,
+  `student_course` varchar(50) NOT NULL,
+  `student_created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`student_id`, `student_first_name`, `student_last_name`, `student_course`, `student_created_at`) VALUES
+(1, 'Jax', 'Evangelio', 'BSIT', '2026-09-21 23:28:00'),
+(2, 'JAN XAVIER', 'EVANGELIO', 'BSA-AGRONOMY', '2026-09-21 23:28:00'),
+(3, 'RON EDMUND', 'EVANGELIO', 'BSEE', '2026-09-21 23:28:00'),
+(4, 'RK', 'FERNANDEZ', 'BSIT', '2026-09-21 23:28:00'),
+(5, 'test', 'test', 'bsit', '2026-09-22 00:19:35');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`book_id`);
+
+--
+-- Indexes for table `borrow`
+--
+ALTER TABLE `borrow`
+  ADD PRIMARY KEY (`borrow_id`),
+  ADD KEY `fk_borrow_student` (`student_id`),
+  ADD KEY `fk_borrow_book` (`book_id`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`student_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `books`
+--
+ALTER TABLE `books`
+  MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `borrow`
+--
+ALTER TABLE `borrow`
+  MODIFY `borrow_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `students`
+--
+ALTER TABLE `students`
+  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `borrow`
+--
+ALTER TABLE `borrow`
+  ADD CONSTRAINT `fk_borrow_book` FOREIGN KEY (`book_id`) REFERENCES `books` (`book_id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_borrow_student` FOREIGN KEY (`student_id`) REFERENCES `students` (`student_id`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
